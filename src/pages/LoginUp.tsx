569a23cc50029ae3a9c9dd370ad267666ae390bf
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-04-30 01:57:32 +0300

    fix: login button label

--- src/pages/LoginUp.tsx
+++ src/pages/LoginUp.tsx
@@ -49,7 +49,7 @@ export const Login: FunctionalComponent = () => {
                 </div>
 
 
-                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login</button>
 
                 <p class="text-sm font-light text-gray-400">
                     Don't have an account yet? <a href="/signup" class="font-medium hover:underline text-primary-500">SignUp here</a>
